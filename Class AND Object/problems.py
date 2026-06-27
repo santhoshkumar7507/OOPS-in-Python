3ea899800b899f7e080bf05a2454197133616f92
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-06-27 13:40:10 +0530

    Class and Object

---
+++ Class AND Object/problems.py
@@ -0,0 +1,23 @@
+# 1. Class and Object
+# Real Life Example
+
+# Blueprint = Class
+
+# Actual House = Object
+
+# problem 1: Student Class
+class Student:
+    pass
+
+s1 = Student()
+
+print(type(s1))
+
+# problem 2: Car Object
+
+class Car:
+    color = "Red"
+
+car1 = Car()
+
+print(car1.color)
